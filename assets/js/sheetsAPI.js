c8b740bb1ab6285ea8d24cb6653acba69e8c6d46
Daniel Mossaband <DanMossa@gmail.com>
2023-05-08 09:47:56 -0700

    Add cart layout

---
+++ assets/js/sheetsAPI.js
@@ -0,0 +1,17 @@
+function getSheetsData() {
+    const breadData = [{
+        "image": "images/pic01.jpg",
+        "name": "Focaccia",
+        "description": "Garlic-rosemary focaccia with garlic-infused olive oil.",
+        "price": "29.99",
+        "count": 6
+    }, {
+        "image": "images/pic01.jpg",
+        "name": "Baguette",
+        "description": "Vis ac commodo adipiscing arcu aliquet.",
+        "price": "19.99",
+        "count": 2
+    }];
+
+    return breadData
+}
