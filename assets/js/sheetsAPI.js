6031888fa90d73e980cba812f76fe9e9f0df0fa5
Daniel Mossaband <DanMossa@gmail.com>
2023-05-16 21:28:17 -0700

    Add base for adding history row

--- assets/js/sheetsAPI.js
+++ assets/js/sheetsAPI.js
@@ -1,6 +1,45 @@
 const anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhZHJmZmJib3J3d3JjdnZhY2V4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM4MzgyNjIsImV4cCI6MTk5OTQxNDI2Mn0.idwkFmYv-8XcHwkUsW_sHRmJOzDh3-vjiZKOleNdZOU";
 
 /**
+ *
+ * @param {{customerName: string, email: string, paymentMethod: string, pickupTime: string}} customerData
+ * @param {Record<string, string>} itemData
+ * @param {string} total
+ * @return {Promise<void>}
+ */
+async function writeBreadLedgerData(customerData, itemData, total) {
+    const nowPST = new Date().getUTCDate().toLocaleString("en-US", {
+        timeZone: "America/Los_Angeles"
+    });
+
+    let productOrder = [];
+
+    // Reduce quantity
+    let reduceQuantityPromises = [];
+    for (const [productName, quantityPurchased] of Object.entries(itemData)) {
+        productOrder.push(`${quantityPurchased}x ${productName}`);
+    }
+
+    const productOrderString = productOrder.join(',');
+
+    // Add order to history
+    const {customerName, email, paymentMethod, pickupTime} = customerData;
+    const addToOrderHistoryPromise = _addToOrderHistory({
+        time: nowPST,
+        name: customerName,
+        email: email,
+        payment: paymentMethod,
+        order: productOrderString,
+        total: total,
+        pickup: pickupTime
+    });
+
+
+    // Execute
+    await Promise.all([addToOrderHistoryPromise, ...reduceQuantityPromises])
+}
+
+/**
  * @return {Promise<{
  *  name: string,
  *  size: string,
@@ -30,7 +69,6 @@ async function getBreadLedgerData() {
             image,
         ] = value;
 
-        console.log(cost);
         data.push({
             name,
             size,
@@ -59,6 +97,28 @@ function itemNamesToDOMIds(itemNames) {
 }
 
 /**
+ * @param {{time: string,
+ * name: string,
+ * email: string,
+ * payment: string,
+ * order: string,
+ * total: string,
+ * pickup: string}} data
+ * @return {Promise<void>}
+ * @private
+ */
+async function _addToOrderHistory(data) {
+    await fetch("https://hadrffbborwwrcvvacex.functions.supabase.co/add-to-order-history", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${anonKey}`
+        },
+        body: JSON.stringify(data),
+    });
+}
+
+/**
  * Expects `values` to return an array with values in the order of
  * Product, Size, Placeholder1, Quantity, Description, Cost, Placeholder2, Image
  *
@@ -75,7 +135,6 @@ async function _fetchRawBreadLedgerData() {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${anonKey}`
         },
-        // body: JSON.stringify(data), /
     });
 
     return res.json();
---
+++ supabase/functions/add-to-order-history/index.ts
@@ -0,0 +1,28 @@
+import {GoogleAPI} from "https://deno.land/x/google_deno_integration/mod.ts";
+
+import {serve} from "https://deno.land/std@0.168.0/http/server.ts"
+import {breadLedgerSheetId, clientEmail, privateKey} from "../_shared/constants.ts";
+import {corsHeaders} from "../_shared/cost.ts";
+
+serve(async (req) => {
+    const {time, name, email, payment, order, total, pickup} = await req.json()
+
+    const api = new GoogleAPI({
+        email: clientEmail,
+        scope: ["https://www.googleapis.com/auth/spreadsheets"],
+        key: privateKey,
+    });
+
+
+    // Google sheet has the layout
+    // [Time, Name, Email, Payment, Order, Total, Pickup]
+    await api.post(`https://sheets.googleapis.com/v4/spreadsheets/${breadLedgerSheetId}/values/History:append?valueInputOption=USER_ENTERED`, {
+        "range": "History",
+        "values": [[time, name, email, payment, order, total, pickup]],
+    });
+
+    return new Response(
+        JSON.stringify(),
+        {headers: {"Content-Type": "application/json", ...corsHeaders}},
+    )
+})
